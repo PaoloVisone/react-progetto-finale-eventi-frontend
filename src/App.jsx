55e4f8c5cd6ec031b7b42bc4af7c4483f3236ecf
PaoloVisone <polvis.pv.22@gmail.com>
2025-07-07 18:17:47 +0200

    Update routing for Booking page to use Booking component instead of BookingForm; improve code organization with clearer comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
+// Import dei moduli React Router per la gestione delle rotte
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// Layouts
+
+// Import dei layout e delle pagine principali dell'applicazione
 import DefaultLayout from "./layouts/DefaultLayout"
-// Pages
 import Home from "./pages/Home";
 import EventDetail from "./pages/EventDetail"
 import Eventi from "./pages/Eventi"
 import ContactsForm from "./pages/ContactsForm"
-import BookingForm from "./pages/BookingForm"
-// Css
+import Booking from "./pages/Booking"
+
+// Import del file CSS principale dell'app
 import './App.css'
 
 function App() {
   return (
+    // Router principale che gestisce la navigazione
     <Router>
       <Routes>
+        {/* Tutte le rotte sono racchiuse dal layout di default */}
         <Route element={<DefaultLayout />} >
+          {/* Rotta per la home page */}
           <Route path="/" element={<Home />} />
+          {/* Rotta per la lista degli eventi */}
           <Route path="/events" element={<Eventi />} />
+          {/* Rotta per il dettaglio di un evento specifico */}
           <Route path="/events/:id" element={<EventDetail />} />
-          <Route path="/events/:id/bookings" element={<BookingForm />} />
+          {/* Rotta per la prenotazione di un evento specifico */}
+          <Route path="/events/:id/bookings" element={<Booking />} />
+          {/* Rotta per la pagina dei contatti */}
           <Route path="/contacts" element={<ContactsForm />} />
         </Route>
       </Routes>
